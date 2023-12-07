ecdc9a8c82678e3da4a0d6b9e7bde6b241f3acd0
ZRL <zrl@example.com>
2023-12-07 21:49:55 +0100

    project

---
+++ UnitConverterd/script.js
@@ -0,0 +1,13 @@
+let inputValue = 10
+milesToKm = true
+result = 0
+if (milesToKm) {
+  result = inputValue * 1.60934;
+} else {
+  result = inputValue / 1.60934;
+}
+const resultString = inputValue + " miles are " + result + " km";
+console.log(resultString)
+
+const resultElement = document.getElementById('resultElement');
+resultElement.innerHTML = resultString;
